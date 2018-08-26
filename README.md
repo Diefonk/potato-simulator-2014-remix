5d21024785723af7e8a6387edbdeefb62738c70c
Diefonk <thediefonk@gmail.com>
2018-08-26 21:05:40 +0200

    Version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# potato-simulator-2014-remix
-A reimagining of the classic game played by no one
+# Potato Simulator 2014 Remix
+A reimagining of the classic game played by no one. Made with [p5.js](https://p5js.org/)
+
+Image: [Russet potato cultivar with sprouts by ZooFari](https://commons.wikimedia.org/wiki/File:Russet_potato_cultivar_with_sprouts.jpg)  
+Font: [Gloria Hallelujah by Kimberly Geswein](https://fonts.google.com/specimen/Gloria+Hallelujah)  
+Music: [Ambler by Kevin MacLeod](https://incompetech.com/music/royalty-free/index.html?isrc=USUAN1300019)
---
+++ script.js
@@ -0,0 +1,65 @@
+var potatoBackground;
+var potato;
+var potatoText;
+var isPotato;
+var countdown;
+var totalTime;
+var bestTime;
+
+function setup() {
+	createCanvas(800, 600);
+	potatoBackground = loadImage("background.png");
+	potato = loadImage("potato.png");
+	textFont("Gloria Hallelujah");
+	textSize(32);
+	potatoText = "Click the potato to start being a potato";
+	isPotato = false;
+	bestTime = 0;
+}
+
+function draw() {
+	if (potatoIsHovered()) {
+		cursor(HAND);
+	} else {
+		cursor(ARROW);
+	}
+
+	if (isPotato) {
+		deltaTime = 1 / frameRate();
+		totalTime += deltaTime;
+		countdown -= deltaTime;
+		if (countdown <= 0) {
+			isPotato = false;
+			document.getElementById("music").pause();
+			if (totalTime > bestTime) {
+				bestTime = totalTime;
+			}
+			potatoText = "You were a potato for " + Math.trunc(totalTime) + " seconds";
+			potatoText += "\nYour best time is " + Math.trunc(bestTime) + " seconds";
+			potatoText += "\nClick the potato to try again";
+		} else {
+			potatoText = "You will stop being a potato in " + Math.trunc(countdown) + " seconds";
+			potatoText += "\nClick the potato to focus on being a potato";
+		}
+	}
+
+	image(potatoBackground, 0, 0);
+	text(potatoText, 50, 450);
+}
+
+function mousePressed() {
+	if (potatoIsHovered()) {
+		if (!isPotato) {
+			isPotato = true;
+			totalTime = 0;
+			document.getElementById("music").play();
+		}
+		countdown = random(10, 60);
+	}
+}
+
+function potatoIsHovered() {
+	return mouseX > 0 && mouseX < width &&
+		mouseY > 0 && mouseY < height &&
+		potato.get(mouseX, mouseY)[3] > 0;
+}
